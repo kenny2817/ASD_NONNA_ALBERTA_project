01d765ef2639c83391dee34a95681ee1a0fbe1ce
kenny281 <kenny2817.qm@gmail.com>
2024-05-30 14:13:36 +0200

    last heuristic

--- solution.cpp
+++ solution.cpp
@@ -1,10 +1,9 @@
-//
-// Created by quent on 26/05/2024.
-//
 #include <fstream>
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <chrono>
+#include <iostream>
 
 #include "nonna.h"
 
@@ -15,22 +14,27 @@ struct centrino {
     float median;
     vector<int> to;
 
-    explicit centrino(float mn = 0) : mean(mn), to({}) {}
+    centrino() : mean(0), median(0) {}
 };
 
 vector<centrino> c;
 ofstream out ("output.txt");
 
 // mean
-bool compareByMean (const int &a, const int &b) {
+inline bool compareByMean (const int &a, const int &b) {
     return c[a].mean < c[b].mean;
 }
 
+// median
+inline bool compareByMedian (const int &a, const int &b) {
+    return c[a].median < c[b].median;
+}
+
 // Median calculation function
-float calculateMedian(vector<int>& values) {
+inline float calculateMedian(vector<int>& values) {
     size_t size = values.size();
-    if (size == 0) return 0; // If there are no elements, return 0
-    sort(values.begin(), values.end()); // Sort the values
+    if (size == 0) return 0;
+    sort(values.begin(), values.end());
 
     if (size % 2 == 0) {
         return (values[size / 2 - 1] + values[size / 2]) / 2.0;
@@ -39,13 +43,8 @@ float calculateMedian(vector<int>& values) {
     }
 }
 
-// median
-bool compareByMedian (const int &a, const int &b) {
-    return c[a].median < c[b].median;
-}
-
 // O (log(G))
-int arbreMagique (int* ftree, int index, int G) {
+int arbreMagique (vector<int>& ftree, int index, int G) {
     // sum
     int sum = 0;
     for (int i = index +2; i <= G; i += i & -i)
@@ -61,16 +60,13 @@ int arbreMagique (int* ftree, int index, int G) {
 // O(F*log(G))
 int count(const vector<int>& order, const int G) {
     int crossings = 0;
-    int* ftree = new int[G +1]();
+    vector<int> ftree(G + 1, 0);
 
     // theta (F)
     for (const auto& i : order)
         for (const int j : c[i].to)
             // O (log(G))
             crossings += arbreMagique(ftree, j, G);
-
-    // Deallocate the memory
-    delete[] ftree;
 
     return crossings;
 }
@@ -86,6 +82,10 @@ void printout (int count, vector<int>& order) {
 }
 
 int main() {
+    // Define the duration for which the loop should run
+    auto start_time = chrono::steady_clock::now();
+    auto duration1 = chrono::milliseconds (2990);
+
     ifstream in ("input.txt");
 
     int C, G, F;
@@ -108,33 +108,38 @@ int main() {
 
         sort(c[i].to.begin(), c[i].to.end());
 
-        c[i].mean /= (float) c[i].to.size();
-
-        c[i].median = calculateMedian(c[i].to);
+        if (!c[i].to.empty()) {
+            c[i].mean /= c[i].to.size();
+            c[i].median = calculateMedian(c[i].to);
+        }
     }
 
+    bool best = false;
     //start
     int minSoFar = count(order, G);
-    printout(minSoFar, order);
 
     // mean heuristic
     sort(order.begin(), order.end(), compareByMean);
-
     int actual = count(order, G);
     if (minSoFar > actual) {
+        best = true;
         minSoFar = actual;
-        printout(minSoFar, order);
     }
 
     // median euristic
     sort(order.begin(), order.end(), compareByMedian);
-
     actual = count(order, G);
     if (minSoFar > actual) {
         minSoFar = actual;
-        printout(minSoFar, order);
-    } else
+    } else if (best) {
         sort(order.begin(), order.end(), compareByMean);
+        minSoFar = count(order, G);
+    } else {
+        sort(order.begin(), order.end());
+        minSoFar = count(order, G);
+    }
+
+    printout(minSoFar, order);
 
     // random generator
     random_device rd;
@@ -142,24 +147,22 @@ int main() {
     uniform_int_distribution<> distribution(0, C -1);
 
     int a,b;
+
     // random
-    while (1) {
+    while (chrono::steady_clock::now() - start_time < duration1) {
         a = distribution(generate);
 
         do b = distribution(generate);
-        while (a == b);
+        while (a == b || abs(a - b) > C /17);
 
         swap(order[a], order[b]);
         actual = count(order, G);
 
-        if (actual < minSoFar) {
-            minSoFar = actual;
-            printout(minSoFar, order);
-            continue;
-        }
-
-        if (actual == minSoFar) continue;
-
-        swap(order[a], order[b]);
+        if (actual < minSoFar)  minSoFar = actual;
+        else                    swap(order[a], order[b]);
     }
+
+    printout(minSoFar, order);
+
+    return 0;
 }
